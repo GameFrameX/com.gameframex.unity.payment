1951672922e4d34bbf8e665cb77185339e8fb9b4
Blank <wangfj11@foxmail.com>
2026-04-20 20:46:39 +0800

    refactor(payment): 合并 Inspector 重复的类型刷新方法
    将 RefreshComponentAndroidTypeNames 和 RefreshComponentIOSTypeNames
    合并为通用的 RefreshTypeNames 方法，消除重复代码。

--- Editor/PaymentComponentInspector.cs
+++ Editor/PaymentComponentInspector.cs
@@ -1,7 +1,7 @@
-﻿// GameFrameX 组织下的以及组织衍生的项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规和许可证的要求。
-// 
+// GameFrameX 组织下的以及组织衍生的项目的版权、商标、专利和其他相关权利均受相应法律法规的保护。使用本项目应遵守相关法律法规的许可证的要求。
+//
 // 本项目主要遵循 MIT 许可证和 Apache 许可证（版本 2.0）进行分发和使用。许可证位于源代码树根目录中的 LICENSE 文件。
-// 
+//
 // 不得利用本项目从事危害国家安全、扰乱社会秩序、侵犯他人合法权益等法律法规禁止的活动！任何基于本项目二次开发而产生的一切法律纠纷和责任，我们不承担任何责任！
 
 using System.Collections.Generic;
@@ -66,8 +66,8 @@ namespace GameFrameX.Payment.Editor
             base.Enable();
             ComponentAndroidType = serializedObject.FindProperty("m_componentAndroidType");
             ComponentIOSType = serializedObject.FindProperty("m_componentIOSType");
-            RefreshComponentAndroidTypeNames(typeof(IPaymentManager));
-            RefreshComponentIOSTypeNames(typeof(IPaymentManager));
+            RefreshTypeNames(ComponentAndroidType, ref ComponentAndroidTypeNames, ref ComponentAndroidTypeNameIndex, typeof(IPaymentManager));
+            RefreshTypeNames(ComponentIOSType, ref ComponentIOSTypeNames, ref ComponentIOSTypeNameIndex, typeof(IPaymentManager));
         }
 
         protected override void RefreshTypeNames()
@@ -75,7 +75,7 @@ namespace GameFrameX.Payment.Editor
             RefreshComponentTypeNames(typeof(IPaymentManager));
         }
 
-        private void RefreshComponentAndroidTypeNames(System.Type type)
+        private void RefreshTypeNames(SerializedProperty property, ref string[] names, ref int index, System.Type type)
         {
             List<string> managerTypeNames = new List<string>
             {
@@ -83,38 +83,15 @@ namespace GameFrameX.Payment.Editor
             };
 
             managerTypeNames.AddRange(Utility.Assembly.GetRuntimeTypeNames(type));
-            ComponentAndroidTypeNames = managerTypeNames.ToArray();
-            ComponentAndroidTypeNameIndex = 0;
-            if (!ComponentAndroidType.stringValue.IsNullOrEmpty())
+            names = managerTypeNames.ToArray();
+            index = 0;
+            if (!property.stringValue.IsNullOrEmpty())
             {
-                ComponentAndroidTypeNameIndex = managerTypeNames.IndexOf(ComponentAndroidType.stringValue);
-                if (ComponentAndroidTypeNameIndex <= 0)
+                index = managerTypeNames.IndexOf(property.stringValue);
+                if (index <= 0)
                 {
-                    ComponentAndroidTypeNameIndex = 0;
-                    ComponentAndroidType.stringValue = null;
-                }
-            }
-
-            serializedObject.ApplyModifiedProperties();
-        }
-
-        private void RefreshComponentIOSTypeNames(System.Type type)
-        {
-            List<string> managerTypeNames = new List<string>
-            {
-                NoneOptionName
-            };
-
-            managerTypeNames.AddRange(Utility.Assembly.GetRuntimeTypeNames(type));
-            ComponentIOSTypeNames = managerTypeNames.ToArray();
-            ComponentIOSTypeNameIndex = 0;
-            if (!ComponentIOSType.stringValue.IsNullOrEmpty())
-            {
-                ComponentIOSTypeNameIndex = managerTypeNames.IndexOf(ComponentIOSType.stringValue);
-                if (ComponentIOSTypeNameIndex <= 0)
-                {
-                    ComponentIOSTypeNameIndex = 0;
-                    ComponentIOSType.stringValue = null;
+                    index = 0;
+                    property.stringValue = null;
                 }
             }
 
